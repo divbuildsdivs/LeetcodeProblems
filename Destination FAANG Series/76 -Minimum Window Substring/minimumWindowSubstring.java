97ac511fc294224601554638f9af80552ee923de
Divyojyoti Ghosh <datasci.divyosmiley9@gmail.com>
2026-06-05 13:42:37 +0100

    Updated minimum window substring

--- Destination FAANG Series/76 -Minimum Window Substring/minimumWindowSubstring.java
+++ Destination FAANG Series/76 -Minimum Window Substring/minimumWindowSubstring.java
@@ -1,66 +1,55 @@
 class Solution {
     public String minWindow(String s, String t) {
-        HashMap<Character, Integer> tmap = new HashMap<>();
+        int left =0, right=0, formed = 0, uniqueChars;
+        String res = "";
+
         HashMap<Character, Integer> smap = new HashMap<>();
-        int resultLength = s.length() + 1;
-        int resultStart = 0;
-        int resultEnd = 0;
-        int right = 0, left = 0;
-        StringBuilder distinctString = new StringBuilder();
-        if (t.length() > s.length()) {
-            return "";
+        HashMap<Character, Integer> tmap = new HashMap<>();
+        for (int i =0; i<t.length(); i++){
+            addNewCharacter(tmap, t.charAt(i));
         }
-        for (int i = 0; i < t.length(); i++) {
-            if (tmap.containsKey(t.charAt(i))) {
-                tmap.replace(t.charAt(i), tmap.get(t.charAt(i)) + 1);
-            } else {
-                tmap.put(t.charAt(i), 1);
-                distinctString = distinctString.append(t.charAt(i));
-            }
-        }
-        while (right < s.length()) {
-            System.out.println("right: " + right + " left: " + left);
-            if (left < resultEnd) {
-                if (smap.get(s.charAt(left)) == 1) {
-                    smap.remove(s.charAt(left));
-                } else {
-                    smap.replace(s.charAt(left), (smap.get(s.charAt(left)) - 1));
-                }
-                smap.forEach((key, value) -> System.out.println("key: " + key + " value: " + value));
-                left++;
-            } else {
-                if (smap.containsKey(s.charAt(right))) {
-                    smap.replace(s.charAt(right), smap.get(s.charAt(right)) + 1);
-                } else {
-                    smap.put(s.charAt(right), 1);
-                }
-            }
-            int matches = 0;
-            while (matches < distinctString.length()
-                && smap.get(distinctString.charAt(matches)) != null
-                && (smap.get(distinctString.charAt(matches)) >= tmap.get(distinctString.charAt(matches)))) {
-                System.out.println("distinctString.charAt(matches): " + distinctString.charAt(matches));
-                System.out.println("smap.get(distinctString.charAt(matches): " + smap.get(distinctString.charAt(matches)));
-                System.out.println("tmap.get(distinctString.charAt(matches): " + tmap.get(distinctString.charAt(matches)));
-                matches++;
-            }
-            if (matches == distinctString.length()) {
-                System.out.println("right- left + 1" + (right - left + 1) + " resultLength: " + resultLength);
-                if ((right - left) < resultLength) {
-                    resultLength = right - left + 1;
-                    resultStart = left;
-                    resultEnd = right;
-                }
-            }
-            if (left == resultEnd) {
+        uniqueChars = tmap.size();
+        while(right < s.length()) {
+            addNewCharacter(smap, s.charAt(right));
+            if(!matchMaps(smap, tmap)) {
                 right++;
             }
+            else if(matchMaps(smap, tmap)){
+                if(res.equals("") || (right - left + 1) < res.length()){
+                    res = s.substring(left, right + 1);
+                }
+                removeCharacter(smap, s.charAt(left));
+                left++;
+            }
         }
-        if (resultLength == s.length() + 1) {
-            return "";
-        } else {
-            return s.substring(resultStart, resultEnd + 1);
+        return res;
+    }
+
+    void addNewCharacter(Map<Character, Integer> map, Character key){
+        Integer val = map.getOrDefault(key, 0);
+        if(val == 0){
+            map.put(key, 1);     
         }
+        else {
+            map.replace(key, val + 1);
+        }
+    }
+    void removeCharacter(Map<Character, Integer> map, Character key){
+        if(map.getOrDefault(key, 0) > 1){
+            map.replace(key, (map.get(key) - 1));
+        }
+        else {
+            map.remove(key);
+        }
+    }
+
+    boolean matchMaps(Map<Character, Integer> smap, Map<Character, Integer> tmap) {
+       for(Map.Entry<Character, Integer> entry: tmap.entrySet()) {
+            if(!smap.containsKey(entry.getKey()) || smap.get(entry.getKey()) < entry.getValue()) {
+                return false;
+            }
+       }
+       return true;
     }
 
 }
