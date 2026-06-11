1b59dda03a281465aa4cebfe568e6c9abdc30e3a
Divyojyoti Ghosh <divyojyoti.ghosh@rxglobal.com>
2026-06-11 05:05:42 +0100

    Refactor minimum window substring solution and remove debug print statements

--- Destination FAANG Series/76 -Minimum Window Substring/optimalApproach.java
+++ Destination FAANG Series/76 -Minimum Window Substring/optimalApproach.java
@@ -1,6 +1,6 @@
 class Solution {
     public String minWindow(String s, String t) {
-        int left =0, right=0, formed = 0, uniqueChars;
+        int left =0, right=0, formed = 0, uniqueChars, required = t.length();
         String res = "";
 
         // System.out.println("Starting minWindow with s='" + s + "' and t='" + t + "'");
@@ -11,7 +11,7 @@ class Solution {
             addNewCharacter(tmap, t.charAt(i));
         }
         uniqueChars = tmap.size();
-        // System.out.println("Target frequency map: " + tmap);
+       // System.out.println("Target frequency map: " + tmap);
         if(s.length() == 0 || t.length() == 0 || s.length() < t.length()) {
             // System.out.println("Edge case: returning empty string.");
             return res;
@@ -21,45 +21,39 @@ class Solution {
             return s;
         }
         else {
-            // System.out.println("Initializing sliding window.");
+           // System.out.println("Initializing sliding window.");
         }   
-        addNewCharacter(smap, s.charAt(right));
         while(right < s.length()) {
-        
-            if(!matchMaps(smap, tmap)) {
-                if (right < s.length()-1) {
-                    right++;
-                    // System.out.println("Window is not valid. Moving right pointer to: " + right);
-                    addNewCharacter(smap, s.charAt(right));
-                    // System.out.println("\nExpanding window: left=" + left + ", right=" + right + ", char='" + s.charAt(right) + "'");
-                    // System.out.println("Current window: '" + s.substring(left, right + 1) + "'");
-                    // System.out.println("Current source map after add: " + smap);
+            // System.out.println(
+            //     "Loop state -> left=" + left + ", right=" + right
+            //     + ", formed=" + formed + ", required=" + required
+            //     + ", window='" + s.substring(left, right + 1) + "'"
+            //     + ", smap=" + smap + ", res='" + res + "'"
+            // );
+            addNewCharacter(smap, s.charAt(right));
+            if(tmap.containsKey(s.charAt(right)) && tmap.get(s.charAt(right)) >= smap.get(s.charAt(right))){
+                    formed++;
                 }
-                else {
-                    // System.out.println("Reached end of string. Cannot expand further.");
-                    break;
-                }
-            }
-            else if(matchMaps(smap, tmap)){
-                // System.out.println("Window is valid: [" + left + ", " + right + "] -> '" + s.substring(left, right + 1) + "'");
-                if(res.equals("") || (right - left + 1) < res.length()){
+            while(formed == required){
+                // System.out.println(
+                //     "Contracting -> left=" + left + ", right=" + right
+                //     + ", formed=" + formed + ", required=" + required
+                //     + ", window='" + s.substring(left, right + 1) + "'"
+                //     + ", smap=" + smap + ", res='" + res + "'"
+                // );
+                if(res.length() == 0 || (right - left + 1) < res.length()){
                     res = s.substring(left, right + 1);
-                    // System.out.println("Updated best result to: '" + res + "'");
                 }
-                if(left < right){
-                    // System.out.println("Shrinking window by removing s[" + left + "]='" + s.charAt(left) + "'");
-                    removeCharacter(smap, s.charAt(left));
-                    left++;
-                    // System.out.println("Shrunk window: left=" + left + ", right=" + right + ". Current window: '" + s.substring(left, right + 1) + "'");
-                    // System.out.println("Current source map after remove: " + smap);
+                removeCharacter(smap, s.charAt(left));
+                if(tmap.containsKey(s.charAt(left)) && tmap.get(s.charAt(left)) > smap.getOrDefault(s.charAt(left), 0)){
+                    formed--;
                 }
-                else {
-                    right++;
-                    // System.out.println("Cannot be shrinked After Expanding: left=" + left + ", right=" + right + ". Moving right pointer.");
-                }
+                left++;  
             }
+
+           right++;
         }
-        // System.out.println("Finished. Final result='" + res + "'");
+       // System.out.println("Finished. Final result='" + res + "'");
         return res;
     }
 
