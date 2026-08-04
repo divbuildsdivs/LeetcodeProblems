eb23bdb3a9fee75f123526eda0bd6b5c1d00df59
Divyojyoti Ghosh <datasci.divyosmiley9@gmail.com>
2026-08-04 16:31:44 +0100

    Implement KthLargest class to find the Kth largest element in a stream

---
+++ Destination FAANG Series/703. Kth Largest Element in a Stream/Solution.java
@@ -0,0 +1,30 @@
+class KthLargest {
+
+    int k;
+    PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+
+    public KthLargest(int k, int[] nums) {
+        this.k = k;
+        for(int i =0; i<nums.length; i++) {
+            minHeap.offer(nums[i]);
+            if(minHeap.size() > k) {
+                minHeap.poll();
+            }
+        }
+        
+    }
+    
+    public int add(int val) {
+         minHeap.offer(val);
+         if(minHeap.size() > k) {
+            minHeap.poll();
+         }
+         return minHeap.peek();
+    }
+}
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest obj = new KthLargest(k, nums);
+ * int param_1 = obj.add(val);
+ */
