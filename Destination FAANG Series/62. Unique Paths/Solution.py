3c849ccb741688a17a88a170f77485c23b435638
Divyojyoti Ghosh <datasci.divyosmiley9@gmail.com>
2026-08-28 14:31:39 +0100

    Implement uniquePaths function for Unique Paths problem

---
+++ Destination FAANG Series/62. Unique Paths/Solution.py
@@ -0,0 +1,10 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        row = [1] * n
+
+        for i in range (1, m):
+            newRow = [1] * n
+            for j in range(n - 2, -1, -1 ):
+                newRow[j] = row[j] + newRow[j+1]
+            row = newRow
+        return row[0]
