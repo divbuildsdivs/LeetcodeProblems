2cf8cdc37e16900ecaa95b05a98d1a6b65981baf
Divyojyoti Ghosh <datasci.divyosmiley9@gmail.com>
2026-09-03 17:44:54 +0100

    Implement isMatch function for Regular Expression Matching problem

---
+++ Destination FAANG Series/10. Regular Expression Matching/Solution.py
@@ -0,0 +1,26 @@
+#solution from neetcode
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+
+        cache = {}   #memoization
+        
+        def dfs(i, j):
+            if(i, j) in cache:
+                return cache[(i, j)]
+            if i >= len(s) and j >= len(p):
+                return True
+            if j >= len(p):
+                return False
+        
+            match =  i < len(s) and (s[i] == p[j] or p[j] == ".")
+            if (j + 1 < len(p)) and p[j+1] == "*":
+                cache[(i,j)] = (dfs(i, j+2) or                       #escape *
+                                (match and dfs(i+1, j)))                         #if character matches, use the *, check both if any of them is true
+                return cache[(i,j)] 
+            if match:
+                cache[(i,j)] = dfs(i+1, j+1)
+                return cache[(i,j)]
+            
+            return False
+        return dfs(0,0)
+        
