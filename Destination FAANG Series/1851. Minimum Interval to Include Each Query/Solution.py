d3da091ca2215886be26a7d7e1e82bf7f20dc137
Divyojyoti Ghosh <datasci.divyosmiley9@gmail.com>
2026-09-10 20:24:08 +0100

    Implements 1851. Minimum Interval to Include Each Query

---
+++ Destination FAANG Series/1851. Minimum Interval to Include Each Query/Solution.py
@@ -0,0 +1,18 @@
+class Solution:
+    def minInterval(self, intervals: List[List[int]], queries: List[int]) -> List[int]:
+        intervals.sort()
+        res = {}
+        i = 0
+        minHeap = []
+        # heapq.heapify(minHeap)
+        for q in sorted(queries):
+            while i < len(intervals) and intervals[i][0] <= q:
+                l, r = intervals[i]
+                heapq.heappush(minHeap,(r - l + 1, r))
+                i += 1
+            while minHeap and minHeap[0][1] < q:
+                heapq.heappop(minHeap)
+            res[q] = minHeap[0][0]  if len(minHeap) > 0 else -1
+       
+        return [res[i] for i in queries]
+        
