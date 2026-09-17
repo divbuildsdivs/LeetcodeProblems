0534265494591be52749d6550b74ca1a87692064
Divyojyoti Ghosh <datasci.divyosmiley9@gmail.com>
2026-09-17 22:17:23 +0100

    Implements leetcode problem 36 Valid Sudoku problem

---
+++ Destination FAANG Series/36. Valid Sudoku/Solution.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isValidSudoku(self, board: list[list[str]]) -> bool:
+        n = len(board)
+        cols = collections.defaultdict(set)
+        rows = collections.defaultdict(set)
+        boxes = collections.defaultdict(set) # key = (r/3, c/3)
+
+        for r in range(n):
+            for c in range(n):
+                if board[r][c] == ".":
+                    continue
+                if(board[r][c] in cols[c]
+                or board[r][c] in rows[r] 
+                or board[r][c] in boxes[(r//3, c//3)]):
+                    return False
+                cols[c].add(board[r][c])
+                rows[r].add(board[r][c])
+                boxes[(r//3, c//3)].add(board[r][c])
+        return True
+
+        
